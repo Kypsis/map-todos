9560f86235f719ebf0f7d79cbdd006b6e92addb1
Harry Sild <puhhbaer@hotmail.com>
2020-01-02 21:04:12 +0200

    add markers functionality

--- src/components/Map/Map.component.tsx
+++ src/components/Map/Map.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Map as LeafletMap,
   Marker,
@@ -10,23 +10,36 @@ import {
 interface Props {}
 
 const Map: React.FC<Props> = () => {
+  const [markers, setMarkers] = useState([
+    [59.43708, 24.745272],
+    [59.44708, 24.735272]
+  ]);
+
+  const addMarker = (e: any) => {
+    console.log(e.latlng.lat);
+    setMarkers([...markers, [e.latlng.lat, e.latlng.lng]]);
+  };
+
   return (
     <LeafletMap
       center={[59.43708, 24.745272]}
       zoom={12}
       zoomControl={false}
       style={{ width: "100%", height: "100vh" }}
+      onClick={addMarker}
     >
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <ZoomControl position="bottomleft" />
-      <Marker position={[59.43708, 24.745272]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <ZoomControl position="bottomright" />
+      {markers.map(marker => (
+        <Marker position={marker} draggable>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      ))}
     </LeafletMap>
   );
 };
