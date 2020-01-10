c4257d51e005762c434aba8af592b2a3aaf66669
Harry Sild <puhhbaer@hotmail.com>
2020-01-10 22:09:26 +0200

    add initial redux functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "./actions";
+import { StoreState } from "./reducers";
 
 import Header from "../src/components/Header/Header.component";
 import Map from "../src/components/Map/Map.component";
 
-const App: React.FC = () => {
+interface Props {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+const _App: React.FC<Props> = props => {
+  const onButtonClick = () => {
+    props.fetchTodos();
+  };
+
+  const renderList = (): JSX.Element[] => {
+    return props.todos.map(todo => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  };
+
   return (
     <div className="App">
       <Header />
       <Map />
+      <div>
+        <button onClick={onButtonClick}>Fetch</button>
+        {renderList()}
+      </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export default connect(mapStateToProps, { fetchTodos })(_App);
---
+++ src/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import { todosReducer } from "./todos.reducer";
+import { Todo } from "../actions";
+
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({ todos: todosReducer });
